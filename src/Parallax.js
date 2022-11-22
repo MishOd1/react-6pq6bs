a6a79a8e0575a05703e2c9e0188a1e4ff3060ebc
MishOd1 <104631140+MishOd1@users.noreply.github.com>
2022-11-22 20:44:51 +0300

    4 files modified

--- src/Parallax.js
+++ src/Parallax.js
@@ -5,7 +5,7 @@ import './Para.css';
 function Para (){
     return(
         <div className="Para">
-          <Parallax pages={4}>
+          <Parallax pages={2}>
             <ParallaxLayer
                 offset={0}
                 speed={2}
@@ -13,10 +13,8 @@ function Para (){
                 backgroundImage: `url("https://img.freepik.com/premium-photo/bitcoin-against-south-african-rand-american-dollar-close-up-image-conceptual-image-digital-crypto-currency-against-world-traditional-currency_133187-1705.jpg?w=1380")`, 
                 backgroundSize:"cover",
                 }}
-            />
-
-            <ParallaxLayer offset={1}>
-                
+            >
+                <button className="parag">Scrolldown</button>
             </ParallaxLayer>
 
         </Parallax>
--- src/footer2.js
+++ src/footer2.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import "./fotter2.css"
-import ReactDOM from 'react-dom';
+
 import { SocialIcon } from 'react-social-icons';
 
 
@@ -10,7 +10,6 @@ function Footer2(){
     <div>
       <SocialIcon className="social" network="twitter" fgColor="white" style={{ height: 30, width: 30 }} url="https://twitter.com/MeshalOdah" />
       <SocialIcon className="social" network="facebook" fgColor="white" style={{ height: 30, width: 30 }} url="https://www.facebook.com/44a44" />
-      <SocialIcon className="social" network="instagram" bgColor="#d6249f" fgColor="black" style={{ height: 30, width: 30 }} url="https://www.instagram.com/binodah1/" />
       <SocialIcon className="social" network="discord" fgColor="white" style={{ height: 30, width: 30 }} url="https://discordapp.com/users/Claw11#5089" />
     </div>
   )
