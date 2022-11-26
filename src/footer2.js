4a13d2404efb15571bd869dfce144d161ac4c3fc
MishOd1 <104631140+MishOd1@users.noreply.github.com>
2022-11-26 15:33:16 +0300

    3 files modified

--- src/footer2.js
+++ src/footer2.js
@@ -8,9 +8,9 @@ import { SocialIcon } from 'react-social-icons';
 function Footer2(){
   return(
     <div>
-      <SocialIcon className="social" network="twitter" fgColor="white" style={{ height: 30, width: 30 }} url="https://twitter.com/MeshalOdah" />
-      <SocialIcon className="social" network="facebook" fgColor="white" style={{ height: 30, width: 30 }} url="https://www.facebook.com/44a44" />
-      <SocialIcon className="social" network="discord" fgColor="white" style={{ height: 30, width: 30 }} url="https://discordapp.com/users/Claw11#5089" />
+      <SocialIcon className="social" network="twitter" fgColor="white" style={{ height: 25, width: 25 }} url="https://twitter.com/MeshalOdah" />
+      <SocialIcon className="social" network="facebook" fgColor="white" style={{ height: 25, width: 25 }} url="https://www.facebook.com/44a44" />
+      <SocialIcon className="social" network="discord" fgColor="white" style={{ height: 25, width: 25 }} url="https://discordapp.com/users/Claw11#5089" />
     </div>
   )
 }
